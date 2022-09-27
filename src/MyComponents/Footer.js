7cb8b1ca7a951f624d6fd00e717c0e54eb14aa1d
Shubham Choubey <ishubham010@gmail.com>
2022-09-27 23:47:45 +0530

    Update

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
-export const Footer = () => {
-  let footerStyle = {
-    position: "absolute",
-    top: "100vh",
-    width: "100%"
-  }
-  return (
-    <footer className="bg-dark text-light py-3" style={footerStyle}>
-      <p className="text-center">
-      Copyright &copy; MyTodoList.com
-      </p>
-    </footer>
-  )
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
 }
